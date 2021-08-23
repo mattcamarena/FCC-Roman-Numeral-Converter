004f68b1f45c73a3ffbf61ae455f23393cc0629c
mattcamarena <camarena.matthew@gmail.com>
2021-08-23 05:01:16 -0500

    Completed project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-README.md
+Free Code Camp's (FCC) Roman Numeral Converter Project
+
+The Project was to create a function that will recieve an integer value and convert it to a roman numeral. 
+
+Given prompt:<b>
+Convert the given number into a roman numeral.<b>
+
+All roman numerals answers should be provided in upper-case.<b>
+
+Link provided to set rules: https://www.mathsisfun.com/roman-numerals.html
--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+/*
+Roman Numeral Converter
+With a map of all the rules I can parse through the number and replace it with its roman numeral equivilant. 
+
+*/
+
 function convertToRoman(num) {
+  if(num <= 0 || num >= 3999 || (num -Math.floor(num) != 0)) return " ";
+
   const basicComb = {
     0:"",1:"I",2:"II",3:"III",4:"IV",5:"V",6:"VI",7:"VII",8:"VIII", 9:"IX",10:"X",20:"XX",30:"XXX",40:"XL",50:"L",60:"LX",70:"LXX",80:"LXXX",90:"XC",100:"C",200:"CC",300:"CCC",400:"CD",500:"D",600:"DC",700:"DCC",800:"DCCC",900:"CM",1000:"M",2000:"MM",3000:"MMM"
   }
@@ -12,4 +20,13 @@ function convertToRoman(num) {
     
   }
  return res;
+}
+
+function startConvert(){
+  document.getElementById("res").innerHTML = convertToRoman(document.getElementById("numBox").value);
+}
+
+function clearText(){
+  document.getElementById("numBox").value = "";
+  document.getElementById("res").innerHTML= "";
 }
